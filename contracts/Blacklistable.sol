0346887db2acc3854f9c9455f6fe8a67dd3d3bd5
craftsangjae <craftsangjae@gmail.com>
2021-10-29 21:49:43 +0900

    추가 기능 1. 블랙리스트 관리 기능 구현

--- contracts/Blacklistable.sol
+++ contracts/Blacklistable.sol
@@ -10,6 +10,13 @@ contract Blacklistable is Ownable {
     event Blacklisted(address indexed target);
     event DeleteFromBlacklist(address indexed target);
 
+    // validation logic
+    modifier verifyBlacklist(address _address) {
+        require(!check(msg.sender), "Sender is Blacklisted");
+        require(!check(_address), "_address is Blacklisted");
+        _;
+    }
+
     // Register the address in the blacklist
     function blacklisting(address _address) public onlyOwner {
         blacklist[_address] = 1;
