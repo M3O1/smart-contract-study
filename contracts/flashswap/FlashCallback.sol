55c8a6debc326ce4ae749b0ea3ca427393f90ebc
craftsangjae <craftsangjae@gmail.com>
2021-11-27 07:48:57 +0900

    FlashSwap 소스코드 작성

---
+++ contracts/flashswap/FlashCallback.sol
@@ -0,0 +1,79 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "./UniswapV2Library.sol";
+import "../uniswapv2/interfaces/IUniswapV2Callee.sol";
+import "../uniswapv2/interfaces/IUniswapV2Pair.sol";
+import "./interfaces/IUniswapV2Router02.sol";
+import "../uniswapv2/interfaces/IERC20.sol";
+
+contract FlashSwaper is IUniswapV2Callee{
+    address public immutable uniFactory;
+    address public immutable sushiFactory;
+
+    uint constant deadline = 1 hours;
+
+    IUniswapV2Router02 immutable uniRouter;
+    IUniswapV2Router02 immutable sushiRouter;
+
+    constructor(address uniRouterAddress, address sushiRouterAddress) public {
+        uniRouter = IUniswapV2Router02(uniRouterAddress);
+        sushiRouter = IUniswapV2Router02(sushiRouterAddress);
+
+        // Factory 주소 가져오기
+        uniFactory = IUniswapV2Router02(uniRouterAddress).factory();
+        sushiFactory = IUniswapV2Router02(sushiRouterAddress).factory();
+    }
+
+    // 콜백 호출만 허용하도록 구성
+    modifier onlyAllowCallback() {
+        address factory = IUniswapV2Pair(msg.sender).factory();
+        require((factory == uniFactory)||(factory == sushiFactory), "ABANDONED");
+        address token0 = IUniswapV2Pair(msg.sender).token0();
+        address token1 = IUniswapV2Pair(msg.sender).token1();
+        require(msg.sender == UniswapV2Library.pairFor(factory, token0, token1), "ABANDONED");
+        _;
+    }
+
+    // UniswapV2Call의 호출
+    function uniswapV2Call(
+        address sender_,
+        uint amount0_,
+        uint amount1_,
+        bytes calldata data_
+    ) override external onlyAllowCallback() {
+        require(sender_ != address(0));
+        require(amount0_ == 0 || amount1_ == 0);
+
+        // true이면, token1을 lending 받은 콜백 요청
+        // false이면, token0을 lending 받은 콜백 요청
+        bool lendToken1 = isLendingToken1(amount0_, amount1_);
+
+        IERC20 token;
+        address[] memory path;
+        {
+            address token0 = IUniswapV2Pair(msg.sender).token0();
+            address token1 = IUniswapV2Pair(msg.sender).token1();
+
+            path = new address[](2);
+            path[0] = lendToken1 ? token1 : token0;
+            path[1] = lendToken1 ? token0 : token1;
+            token = IERC20(lendToken1 ? token1 : token0);
+        }
+        uint amountToken = lendToken1 ? amount1_ : amount0_;
+        token.approve(address(sushiRouter), amountToken);
+
+        // Uniswap에 갚아야 하는 금액
+        uint amountRequired = UniswapV2Library.getAmountsIn(uniFactory, amountToken, path)[0];
+        // sushiswap으로부터 받는 금액
+        uint amountReceived = sushiRouter.swapExactTokensForTokens(amountToken, amountRequired, path, msg.sender, block.timestamp + deadline)[1];
+
+        // Profit Earned
+        token.transfer(sender_, amountReceived - amountRequired);
+    }
+
+    function isLendingToken1(uint amount0, uint amount1) private returns (bool) {
+        return amount1 != 0;
+    }
+
+}
