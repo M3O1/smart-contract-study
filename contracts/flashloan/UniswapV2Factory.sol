b2cb69e46a5f91e0044b2889995213407fd632c3
craftsangjae <craftsangjae@gmail.com>
2021-11-26 10:24:22 +0900

    FlashLoan Logic 구성하기

---
+++ contracts/flashloan/UniswapV2Factory.sol
@@ -0,0 +1,51 @@
+pragma solidity ^0.8.0;
+
+import './interfaces/IUniswapV2Factory.sol';
+import './UniswapV2Pair.sol';
+
+contract UniswapV2Factory is IUniswapV2Factory {
+    address public override feeTo;
+    address public override feeToSetter;
+
+    mapping(address => mapping(address => address)) public override getPair;
+    address[] public override allPairs;
+
+    constructor(address _feeToSetter) public {
+        feeToSetter = _feeToSetter;
+    }
+
+    function allPairsLength() external override view returns (uint) {
+        return allPairs.length;
+    }
+
+    // 이걸로 어떻게 생성하는지 보자. https://solidity-by-example.org/app/create2/
+    // EIP-1014 SPEC을 활용한 것 ( CREATE2 opcode)
+    // 콘스탄티노플 하드포크 이전 : NOT YET DEPLOYED, DEPLOYED, SELF-DESTRUCTED
+    // 콘스탄티노플 하드포크 이후 : NOT YET DEPLOYED, DEPLOYED, SELF-DESTRUCTED, REDEPLOYED
+    function createPair(address tokenA, address tokenB) external override returns (address pair) {
+        require(tokenA != tokenB, 'UniswapV2: IDENTICAL_ADDRESSES');
+        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        require(token0 != address(0), 'UniswapV2: ZERO_ADDRESS');
+        require(getPair[token0][token1] == address(0), 'UniswapV2: PAIR_EXISTS'); // single check is sufficient
+        bytes memory bytecode = type(UniswapV2Pair).creationCode;
+        bytes32 salt = keccak256(abi.encodePacked(token0, token1));
+        assembly {
+            pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
+        }
+        IUniswapV2Pair(pair).initialize(token0, token1);
+        getPair[token0][token1] = pair;
+        getPair[token1][token0] = pair; // populate mapping in the reverse direction
+        allPairs.push(pair);
+        emit PairCreated(token0, token1, pair, allPairs.length);
+    }
+
+    function setFeeTo(address _feeTo) override external {
+        require(msg.sender == feeToSetter, 'UniswapV2: FORBIDDEN');
+        feeTo = _feeTo;
+    }
+
+    function setFeeToSetter(address _feeToSetter) override external {
+        require(msg.sender == feeToSetter, 'UniswapV2: FORBIDDEN');
+        feeToSetter = _feeToSetter;
+    }
+}
